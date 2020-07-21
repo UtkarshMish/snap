5fe16fd9014bb4109986b7546487d85bbfe7b907
UtkarshMish <iiutkarshmishraii@gmail.com>
2020-07-21 21:09:18 +0530

    added Formik

--- src/Authentication/LogIn/index.jsx
+++ src/Authentication/LogIn/index.jsx
@@ -1,47 +1,58 @@
-import React, { useState } from "react";
+import React from "react";
 import { View, ScrollView, Text, TextInput, Alert } from "react-native";
+
 import styles from "./styles";
 import UtilityButton from "../../Components/Buttons";
 import colors from "../../../config/colors";
+import { Formik } from "formik";
 
 const LogIn = () => {
-  const handleLogin = () => {
+  const handleLogin = ({ username, password }) => {
     Alert.alert(
       "Success !",
       `Username: ${username} and Password: ${password}`,
-      [{ text: "No" }, { text: "Yes" }]
+      [{ text: "Ok" }]
     );
   };
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
   return (
     <ScrollView style={styles.container}>
       <View style={styles.inputContainer}>
         <Text style={styles.mainTitle}>LOG IN</Text>
-        <TextInput
-          clearButtonMode="always"
-          textContentType="emailAddress"
-          keyboardType="email-address"
-          style={styles.textStyle}
-          onChangeText={(username) => setUsername(username)}
-          placeholder="Username"
-          autoCapitalize="none"
-          autoCorrect={false}
-          autoFocus={true}
-        />
-        <TextInput
-          clearButtonMode="always"
-          secureTextEntry={true}
-          textContentType="password"
-          onChangeText={(password) => setPassword(password)}
-          style={styles.textStyle}
-          autoCorrect={false}
-          placeholder="password"
-        />
-      </View>
-      <View style={styles.buttonContainer}>
-        <UtilityButton onPress={handleLogin}>Login</UtilityButton>
-        <UtilityButton color={colors.switchTrue}>Register</UtilityButton>
+        <Formik
+          initialValues={{ username: "", password: "" }}
+          onSubmit={handleLogin}
+        >
+          {({ handleSubmit, handleChange }) => (
+            <>
+              <TextInput
+                clearButtonMode="always"
+                textContentType="emailAddress"
+                keyboardType="email-address"
+                style={styles.textStyle}
+                onChangeText={handleChange("username")}
+                placeholder="Username"
+                autoCapitalize="none"
+                autoCorrect={false}
+                autoFocus={true}
+              />
+              <TextInput
+                clearButtonMode="always"
+                secureTextEntry={true}
+                textContentType="password"
+                onChangeText={handleChange("password")}
+                style={styles.textStyle}
+                autoCorrect={false}
+                placeholder="password"
+              />
+              <View style={styles.buttonContainer}>
+                <UtilityButton onPress={handleSubmit}>Login</UtilityButton>
+                <UtilityButton color={colors.switchTrue}>
+                  Register
+                </UtilityButton>
+              </View>
+            </>
+          )}
+        </Formik>
       </View>
     </ScrollView>
   );
--- src/Authentication/Register/index.jsx
+++ src/Authentication/Register/index.jsx
@@ -1,81 +1,94 @@
-import React, { useState } from "react";
+import React from "react";
 import { View, ScrollView, Text, TextInput, Alert } from "react-native";
 import styles from "./styles";
 import UtilityButton from "../../Components/Buttons";
 import colors from "../../../config/colors";
+import { Formik } from "formik";
 const Register = () => {
-  const handleRegister = () => {
-    Alert.alert(
-      "Success !",
-      `Username: ${username} and Password: ${password}`,
-      [{ text: "No" }, { text: "Yes" }]
-    );
+  const handleRegister = ({
+    name,
+    phone,
+    email,
+    password,
+    confirmPassword,
+  }) => {
+    Alert.alert("Success !", `Username: ${email} and Password: ${password}`, [
+      { text: "Ok" },
+    ]);
   };
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [name, setName] = useState("");
-  const [phone, setPhone] = useState("");
   return (
     <ScrollView style={styles.container}>
       <View style={styles.inputContainer}>
         <Text style={styles.mainTitle}>SIGN UP</Text>
-        <TextInput
-          clearButtonMode="always"
-          textContentType="name"
-          keyboardType="default"
-          style={styles.textStyle}
-          onChangeText={(name) => setName(name)}
-          placeholder="Name"
-          autoCapitalize="words"
-          autoCorrect={false}
-          autoFocus={true}
-        />
-        <TextInput
-          clearButtonMode="always"
-          textContentType="telephoneNumber"
-          keyboardType="number-pad"
-          style={styles.textStyle}
-          onChangeText={(phone) => setPhone(phone)}
-          placeholder="Mobile Number"
-          autoCapitalize="none"
-          autoCorrect={false}
-        />
-        <TextInput
-          clearButtonMode="always"
-          textContentType="emailAddress"
-          keyboardType="email-address"
-          style={styles.textStyle}
-          onChangeText={(username) => setUsername(username)}
-          placeholder="Username"
-          autoCapitalize="none"
-          autoCorrect={false}
-        />
-        <TextInput
-          clearButtonMode="always"
-          secureTextEntry={true}
-          textContentType="password"
-          onChangeText={(password) => setPassword(password)}
-          style={styles.textStyle}
-          autoCorrect={false}
-          placeholder="Password"
-        />
-        <TextInput
-          clearButtonMode="always"
-          secureTextEntry={true}
-          textContentType="password"
-          onChangeText={(confirmPassword) =>
-            setConfirmPassword(confirmPassword)
-          }
-          style={styles.textStyle}
-          autoCorrect={false}
-          placeholder="Confirm Password"
-        />
-      </View>
-      <View style={styles.buttonContainer}>
-        <UtilityButton color={colors.switchTrue} onPress={handleRegister}>
-          Sign Up
-        </UtilityButton>
+        <Formik
+          initialValues={{
+            name: "",
+            phone: "",
+            email: "",
+            password: "",
+            confirmPassword: "",
+          }}
+          onSubmit={handleRegister}
+        >
+          {({ handleChange, handleSubmit }) => (
+            <>
+              <TextInput
+                clearButtonMode="always"
+                textContentType="name"
+                keyboardType="default"
+                style={styles.textStyle}
+                onChangeText={handleChange("name")}
+                placeholder="Name"
+                autoCapitalize="words"
+                autoCorrect={false}
+                autoFocus={true}
+              />
+              <TextInput
+                clearButtonMode="always"
+                textContentType="telephoneNumber"
+                keyboardType="number-pad"
+                style={styles.textStyle}
+                onChangeText={handleChange("phone")}
+                placeholder="Mobile Number"
+                autoCapitalize="none"
+                autoCorrect={false}
+              />
+              <TextInput
+                clearButtonMode="always"
+                textContentType="emailAddress"
+                keyboardType="email-address"
+                style={styles.textStyle}
+                onChangeText={handleChange("email")}
+                placeholder="Username"
+                autoCapitalize="none"
+                autoCorrect={false}
+              />
+              <TextInput
+                clearButtonMode="always"
+                secureTextEntry={true}
+                textContentType="password"
+                onChangeText={handleChange("password")}
+                style={styles.textStyle}
+                autoCorrect={false}
+                placeholder="Password"
+              />
+              <TextInput
+                clearButtonMode="always"
+                secureTextEntry={true}
+                textContentType="password"
+                onChangeText={handleChange("confirmPassword")}
+                style={styles.textStyle}
+                autoCorrect={false}
+                placeholder="Confirm Password"
+              />
+              <View style={styles.buttonContainer}>
+                <UtilityButton color={colors.switchTrue} onPress={handleSubmit}>
+                  Sign Up
+                </UtilityButton>
+              </View>
+            </>
+          )}
+        </Formik>
       </View>
     </ScrollView>
   );
