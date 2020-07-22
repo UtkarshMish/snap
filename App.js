305cc7c69a0aa0be3115c86560028dbec636825c
UtkarshMish <iiutkarshmishraii@gmail.com>
2020-07-22 10:11:49 +0530

    fixed styles extensions

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { View } from "react-native";
-
 import styles from "./AppStyles";
 import StartScreen from "./src/StartScreen";
 import Viewer from "./src/Viewer";
