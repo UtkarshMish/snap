d244359867c3e59d0e52c3715b2e190e18334a40
UtkarshMish <iiutkarshmishraii@gmail.com>
2020-07-23 17:36:21 +0530

    fixed Music Player

--- src/Components/Player/index.js
+++ src/Components/Player/index.js
@@ -1,26 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 import { Icon } from 'react-native-elements';
 import playerStyles from './playerStyles';
 import { Audio } from 'expo-av';
 import { playerTime } from '../../Utilities/timeMethods';
-const Player = ({ URI, size = 56,
+
+const Player = ({
+  URI, size = 56, autoPlay = false,
   style, ...rest
 }) =>
 {
   const [status, setStatus] = useState(false);
   const [music, setMusic] = useState(null);
   const [positionMillis, setPositionMillis] = useState(0);
+  const [totalDuration, setTotalDuration] = useState(0);
+  useEffect(() =>
+  {
+    const Timer = setTimeout(() => getTime(), 1000);
+    return () => clearInterval(Timer);
+  }, [positionMillis, status]);
+
   const iconType = status == false ? "play" : "pause";
   const pauseMusic = async () =>
   {
-    if (status == false && music == null) {
-      const { sound, status: { isPlaying } } = await Audio.Sound.createAsync({ uri: URI }, { shouldPlay: true });
-      setMusic(sound);
-      setStatus(isPlaying);
-
-    }
-    else if (status == false && music != null) {
+    if (status == false && music != null) {
       const { isPlaying } = await music.playAsync();
       setStatus(isPlaying);
     }
@@ -32,8 +35,10 @@ const Player = ({ URI, size = 56,
   const getTime = async () =>
   {
     if (music) {
-      const { positionMillis } = await music.getStatusAsync();
-      setPositionMillis(positionMillis);
+      const status = await music.getStatusAsync();
+      console.log(status);
+      setPositionMillis(status.positionMillis);
+      setTotalDuration(status.durationMillis);
     }
   }
 
@@ -42,10 +47,18 @@ const Player = ({ URI, size = 56,
     const { positionMillis } = await music.getStatusAsync();
     const status = await music.setPositionAsync(positionMillis + (posInSec * 1000));
   }
-  setTimeout(getTime, 1000);
+  const startMusic = async () =>
+  {
+    if (music == null) {
+      const { sound, status: { isPlaying } } = await Audio.Sound.createAsync({ uri: URI }, { shouldPlay: autoPlay });
+      setMusic(sound);
+      setStatus(isPlaying);
+    }
+  }
+  startMusic();
   return (
     <View style={[style, playerStyles.container]} {...rest} >
-      <Text>  {playerTime(positionMillis)} </Text>
+      <Text>  {playerTime(positionMillis) + " / " + playerTime(totalDuration)} </Text>
       <Icon name="backward" type="font-awesome" onPress={() => seekMusic(-15)} size={size} />
       <Icon name={iconType} type="font-awesome" onPress={pauseMusic} size={size} />
       <Icon name="forward" type="font-awesome" onPress={() => seekMusic(15)} size={size} />
--- src/Utilities/timeMethods.js
+++ src/Utilities/timeMethods.js
@@ -9,5 +9,5 @@ export function playerTime(duration)
   minutes = (minutes < 10) ? "0" + minutes : minutes;
   seconds = (seconds < 10) ? "0" + seconds : seconds;
 
-  return `${hours}:${minutes}:${seconds}`;
+  return `${minutes}:${seconds}`;
 }
